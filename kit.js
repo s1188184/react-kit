3192b2d2717016207a32ea2971f0356750ce3518
s1188184 <junk@othe.us>
2019-04-30 20:59:42 -0400

    added meal tracking. booyah.

--- kit.js
+++ kit.js
@@ -13,14 +13,23 @@ export default class KIT extends Component {
       name: 'Kings Island Visitors',
       personRides: {},
       currentDrinks: 3,
-      availableDiners: ['Jenny', 'Ally', 'Ryan', 'Logan', 'Nolan'],
-      currentDiners: ['Jenny', 'Ally', 'Ryan', 'Logan', 'Nolan']
+      // availableDiners: ['Jenny', 'Ally', 'Ryan', 'Logan', 'Nolan'],
+      // currentDiners: ['Jenny', 'Ally', 'Ryan', 'Logan', 'Nolan']
+      diners: {
+        Jenny: true,
+        Allison: true,
+        Ryan: true,
+        Logan: true,
+        Nolan: true
+      }
     };
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
     this.handleDrinkChange = this.handleDrinkChange.bind(this);
     this.handleAddDrinks = this.handleAddDrinks.bind(this);
+    this.handleDinerChange = this.handleDinerChange.bind(this);
+    this.handleAddMeal = this.handleAddMeal.bind(this);
   }
 
   componentDidMount() {
@@ -46,6 +55,14 @@ export default class KIT extends Component {
       debugger;
       this.setState({
         drinks
+      });
+    });
+
+    const mealsRef = firebase.database().ref('temp/meals');
+    mealsRef.on('value', (snapshot) => {
+      let meals = snapshot.val();
+      this.setState({
+        meals
       });
     });
 
@@ -94,15 +111,38 @@ export default class KIT extends Component {
     drinksRef.push({
       submitter: this.state.user.displayName,
       drinks: this.state.currentDrinks,
+      time: new Date()
     });
   }
 
-  handleDinerChange(e) {
+  handleDinerChange(e, diner) {
 
+    const checked = e.target.checked;
+    //console.log(diner + ' is ' + checked);
+
+    this.setState(s => {
+      let newDiners = s.diners;
+      newDiners[diner] = checked;
+      return { diners: newDiners };
+    })
   }
 
   handleAddMeal() {
 
+    const arrayOfDiners = Object.keys(this.state.diners).filter(diner => {
+      return this.state.diners[diner];
+    });
+
+
+    const mealsRef = firebase.database().ref('temp/meals');
+
+    for (let diner of arrayOfDiners) {
+      mealsRef.push({
+        submitter: this.state.user.displayName,
+        diner,
+        time: new Date()
+      });
+    }
   }
 
   render() {
@@ -119,7 +159,11 @@ export default class KIT extends Component {
       drinks = drinkArray.map((d, index) => <div key={index}>{d.submitter} - {d.drinks}</div>);
     }
 
-
+    let meals = [];
+    if (this.state.meals) {
+      const mealArray = Object.values(this.state.meals);
+      meals = mealArray.map((m, index) => <div key={index}>{m.time} {m.submitter} - {m.diner}</div>);
+    }
 
 
     return (
@@ -129,7 +173,7 @@ export default class KIT extends Component {
           this.state.user ?
             <div>
 
-              
+
               <img src={this.state.user.photoURL} height={50} width={50} onClick={this.logout} />
 
 
@@ -142,21 +186,25 @@ export default class KIT extends Component {
                 <h1>Add Drinks</h1>
                 <input type="number" value={this.state.currentDrinks} onChange={this.handleDrinkChange} />
                 <button onClick={this.handleAddDrinks}>Add Drinks</button>
-                {drinks}
+                <p>{drinks}</p>
               </div>
 
 
 
               <div>
                 <h1>Add Meals</h1>
-                {/*this.state.availableDiners.map((diner, index) => {
+                {Object.keys(this.state.diners).map((diner) => {
+                  const value = this.state.diners[diner];
                   return (
-                    <div>
-                      <input type="checkbox" /> {diner}
+                    <div key={diner}>
+                      <input type="checkbox" checked={value} onChange={(e) => this.handleDinerChange(e, diner)} /> {diner}
                     </div>
                   );
-                })*/}
-                coming soon...
+                })}
+                <button onClick={this.handleAddMeal}>Add Meal</button>
+                <p>
+                {meals}
+                </p>
               </div>
 
 
